d64f52f1158f94c54e24ef2584ed9af91b307246
Salvatore <salvatorebono2001@gmail.com>
2023-10-16 16:35:16 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import TheFooter from "./components/TheFooter.vue";
+import TheHeader from "./components/TheHeader.vue";
+import TheMain from "./components/TheMain.vue";
+
+export default {
+  components: {
+    TheHeader,
+    TheMain,
+    TheFooter,
+  },
+};
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+  <TheMain></TheMain>
+  <TheFooter></TheFooter>
+</template>
+
+<style lang="scss"></style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "bootstrap/dist/js/bootstrap.js"
+import { createApp } from 'vue'
+import './scss/general.scss'
+import App from './App.vue'
+
+createApp(App).mount('#app')
